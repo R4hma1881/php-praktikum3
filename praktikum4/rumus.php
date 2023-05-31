e5303ba768d4827e0cfec309394371f54671b8a5
R4hma1881 <rahmadiannurhikma0>
2023-05-31 10:08:28 +0700

    upload tugas 10

---
+++ praktikum4/rumus.php
@@ -0,0 +1,17 @@
+<?php
+
+function luasLingkaran($jariJari)
+{
+    define("PHI",3,14);
+    echo PHI * $jariJari * $jariJari;
+}
+
+function luasPesegi($sisi)
+{
+    echo $sisi * $sisi;
+}
+
+function luasSegitiga($alas, $tinggi)
+{
+    echo 1/2 * $alas * $tinggi;
+}
